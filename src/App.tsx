84b8398160d4c688122f415114101488ea6e4b4e
gpitt-ctrl <gfbpitt@gmail.com>
2025-02-18 15:51:18 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,7 @@ function App(): React.JSX.Element {
                 UM COS420 with React Hooks and TypeScript
             </header>
             <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
+                Test by George P.
             </p>
         </div>
     );
